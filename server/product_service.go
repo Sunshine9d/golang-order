779b5c69b1b9603e490b21e6dc3a3445fe4f1cc3
do.nguyen <nvdo9941@gmail.com>
2025-02-24 16:20:48 +0700

    feat(core): update logic

---
+++ server/product_service.go
@@ -0,0 +1,23 @@
+package server
+
+import (
+	"context"
+	"log"
+
+	pb "golang-order/gen"
+)
+
+type ProductService struct {
+	pb.UnimplementedProductServiceServer
+}
+
+// GetProduct implements gRPC service
+func (s *ProductService) GetProduct(ctx context.Context, req *pb.GetProductRequest) (*pb.Product, error) {
+	log.Println("Received gRPC request for product ID:", req.Id)
+	return &pb.Product{
+		Id:          req.Id,
+		Name:        "Laptop",
+		Description: "A high-performance laptop",
+		Price:       1200.50,
+	}, nil
+}
